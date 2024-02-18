39c5c94e6095630bee7ca131dfa33cbb9b23260f
yichenlionelwu <yichenlionelwu@gmail.com>
2024-02-17 18:31:54 -0800

    website controls

--- motion-detect/client/website/index.js
+++ motion-detect/client/website/index.js
@@ -1,19 +1,26 @@
-API_URL = "http://98.42.152.32:2500/"
+API_URL = "http://98.42.152.32:2500"
 var statbox = document.getElementById("status-box")
 
 console.log("Script loaded")
 
+/*
+
 Notification.requestPermission((result) => {
     console.log(result);
 });
+*/
+
 
 setInterval(() => {
     getStatus()
 }, 1000);
 
 function getStatus() {
-    fetch(API_URL).then((resp) => resp.json()).then((json) => { changeBox(json) })
+    fetch(API_URL).then((resp) => resp.json()).then((json) => {
+        changeBox(json)
+    })
 }
+
 function changeBox(json) {
     if (json.motion === null) {
         statbox.classList.remove("red")
@@ -33,9 +40,25 @@ function changeBox(json) {
         statbox.classList.add("red")
         newNotification("Motion Detected")
     }
+
     function newNotification(content) {
         console.log("sending notif")
-        const notification = new Notification("Motion Detect", { body: content});
+        const notification = new Notification("Motion Detect", {body: content});
         new Notification("helo")
     }
 }
+
+window.addEventListener("keydown", (event) => {
+    console.log("Key pressed " + event.key)
+    if (event.key === "r") {
+        fetch(`${API_URL}/renew`)
+    }
+    if (event.key === "n") {
+        fetch(`${API_URL}/start`)
+    }
+    if (event.key === "m") {
+        fetch(`${API_URL}/stop`)
+    }
+
+    // do something
+});
